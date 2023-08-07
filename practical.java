1729ceffa1a351b568f6a3c15b3e981cb1826c8d
Sandhya-Budili <140372944+Sandhya-Budili@users.noreply.github.com>
2023-08-07 11:21:55 +0530

    Add files via upload

---
+++ practical.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class practical
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int x=s.nextInt();
+    int y=s.nextInt();
+    int z=x+y;
+    System.out.println(z);
+  }
+}
+
